b1f095eb3c261dd723a07656d021c9db11e93419
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-27 18:27:27 +0900

    Feat: hero carousel is DONE

--- pride-land/src/components/HeroCarousel.tsx
+++ pride-land/src/components/HeroCarousel.tsx
@@ -1,34 +1,23 @@
-"use client";
-
 import { HeroCarouselProps } from "../App";
 import { useState } from "react";
 
 
-
-
 export default function HeroCarousel({images}: HeroCarouselProps) {
-let [currentSlide, setCurrentSlide] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
 
-let nextSlide = () => {
-  if (currentSlide === images.length - 1) {
-    setCurrentSlide(0); 
-  }else {
-    setCurrentSlide(currentSlide + 1);
-  }
-}
 
-  let previousSlide = () => {
-    if (currentSlide === 0) {
-      setCurrentSlide(images.length - 1);
-    } else {
-      setCurrentSlide(currentSlide - 1);
-    }
-  }
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
+  };
+
+  const previousSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? images.length - 1 : prevSlide - 1));
+  };
 
 
   return (
     <div className="overflow-hidden relative">
-    <div className="flex size-fit transition ease-out duration-300">
+  <div className="flex transition ease-out duration-700" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
         {images.map((image, index) => (
             <img key={index} src={image.src} alt={image.alt} />
         ))}
@@ -40,7 +29,7 @@ let nextSlide = () => {
                 id={image.alt} 
                 className="w-10 h-10 rounded-full" 
                 aria-current={index === currentSlide ? "true" : "false"} 
-                aria-label={image.alt} 
+                aria-label={`Got to slide ${index + 1}`} 
                 onClick={() => setCurrentSlide(index)}
             >
                 <svg 
@@ -49,17 +38,15 @@ let nextSlide = () => {
                     xmlns="http://www.w3.org/2000/svg"
                 > 
                     {index === currentSlide ? (
-                        <circle r="15" cx="20" cy="20" fill="black" stroke="black" strokeWidth="1" opacity="0.8" />
+                        <circle r="15" cx="20" cy="20" fill="white" opacity="0.9" />
                     ) : (
-                        <circle r="15" cx="20" cy="20" fill="black" stroke="black" strokeWidth="1" opacity="0.5" />
+                        <circle r="15" cx="20" cy="20" fill="white"  opacity="0.6" />
                     )}
                 </svg>
             </button>
         ))}
     </div>
-</div>
-
-    /* <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+<button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={previousSlide} aria-label="Previous Slide">
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -67,13 +54,14 @@ let nextSlide = () => {
             <span className="sr-only">Previous</span>
         </span>
     </button>
-    <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+    <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" onClick={nextSlide} aria-label="Next Slide">
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
             <span className="sr-only">Next</span>
         </span>
-    </button> */
+    </button> 
+    </div>
       );
 }
