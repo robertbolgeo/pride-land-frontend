1c6ac095c2918cd36b0b76624d093b22bb859140
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-27 09:35:56 +0900

    Feat: Initial Navbar commit

--- pride-land/src/App.tsx
+++ pride-land/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar></Navbar>
      <h1 className="text-3xl font-bold underline text-red-400">
     Hello world!
   </h1>
