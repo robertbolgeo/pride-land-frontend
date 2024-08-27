2cc54aa69e6d67043fa1306ef8c006ba4711637c
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-27 11:23:04 +0900

    Feat: Navbar complete. TODO: graphics need updating and hrefs empty

--- pride-land/tailwind.config.js
+++ pride-land/tailwind.config.js
@@ -3,8 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      primary: "#2B673B",
-      secondary: "#F8EE53",
+      'primary': '#2B673B',
+      'secondary': '#F8EE53',
+      'tertiary': '#56CD75',
     },
     extend: {},
   },
