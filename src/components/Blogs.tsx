888294a6c6e8e16fbf69cf2d4617283124e8c728
karl komeya <karlkomeya04@gmail.com>
2024-08-29 12:08:18 +0900

    chore: map to show photos and adjust css

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,38 +1,50 @@
 import { useEffect, useState } from "react";
-import * as blogsApi from '../api/blogs'
-import BlogsTypes from '../interfaces/BlogsType' 
+import * as blogsApi from '../api/blogs';
+import BlogsTypes from '../interfaces/BlogsType';
 
-const  Blogs = () => {
+interface ImageType {
+   src: string,
+   alt: string
+}
 
-      const [blogs, setBlogs ] = useState<BlogsTypes[]>([]);
+interface Props {
+   images: ImageType []
+}
 
-      useEffect(() => {
-         fetchAllBlogs();
-      },[]);
+const Blogs: React.FC<Props> = (props) => {
+  const [blogs, setBlogs ] = useState<BlogsTypes[]>([]);
 
-      const fetchAllBlogs = async() => {
-         const response = await blogsApi.fetchAllBlogs();
-         setBlogs(response)
-      }
+    useEffect(() => {
+       fetchAllBlogs();
+    },[]);
 
-      const createBlogs = blogs.map((blog, index) => {
-      return <div id={'blogs-' + String(index+1)} key={blog.text + index}>
-               <div>
-                  <div className="bg-white my-4 p-2 rounded-md">
-                     <div className="mb-3 text-center">{blog.name}</div>
-                     <div className="p-3">{blog.text}</div>
-                  </div>
-               </div>
-            </div> 
+    const fetchAllBlogs = async() => {
+      const response = await blogsApi.fetchAllBlogs();
+        setBlogs(response)
+    }
+
+    const createBlogs = blogs.map((blog, index) => {
+      return <div className="" id={'blogs-' + String(index+1)} key={blog.text + index}>
+        <div className="">  
+          <div className=" ">
+                <div className="bg-white my-4 shadow-md p-2 rounded-lg">
+                <img className="rounded-lg px-20 mt-5 " src={props.images[index].src}></img>
+                <div className="my-2 text-center">{blog.name}</div>
+                <div className="p-3">{blog.text}</div>
+              </div>
+              </div>
+          </div>
+        </div> 
    })
 
    return (
-      <div className="bg-green-100 mx-5 mt-36 p-5 mb-0 rounded-md"> 
-      <div id='all-blogs'>
-         <div>
-         {createBlogs}
-         </div>
-      </div>
+      
+      <div className="bg-green-100 mt-36 p-32 mb-0 shadow-lg rounded-md mx-auto max-w-7xl px-6 lg:px-8 "> 
+        <div id='all-blogs'>
+          <div>
+            {createBlogs}
+          </div>
+        </div>
       </div>
    )
 }
