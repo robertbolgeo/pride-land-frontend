9bd56e6b82a7fefd2f430a51c9f5d05700a0b7b2
susheepls <169642127+susheepls@users.noreply.github.com>
2024-09-01 18:03:05 +0900

    Fix: change type to FormData from any

--- src/api/volunteers.tsx
+++ src/api/volunteers.tsx
@@ -1,6 +1,6 @@
 const endpoint = process.env.backend_url + 'volunteers/'
 
-export const postVolunteer = async (formData: any) => {
+export const postVolunteer = async (formData: FormData) => {
     try{
         const request = await fetch(endpoint, {
             method: "POST",
