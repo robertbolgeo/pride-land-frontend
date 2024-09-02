d4e9610dfe79dc043a6b5aa2adc0d163113f7dca
susheepls <169642127+susheepls@users.noreply.github.com>
2024-09-02 17:13:11 +0900

    Chore: remove unused imports

--- src/Admin/admin-pages/Volunteer.tsx
+++ src/Admin/admin-pages/Volunteer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as volunteerApi from '../admin-api/admin-volunteers'
 import AdminVolunteer from '../admin-interface/AdminVolunteerType';
 
