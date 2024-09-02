3292c19438b1d970d73357a82e0c4067dde21d76
karl komeya <karlkomeya04@gmail.com>
2024-09-02 14:39:07 +0900

    feat: created admin post blog

---
+++ src/Admin/admin-components/AdminComments.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Comments from '../admin-pages/Comments'
+
+const AdminComments = () => {
+  return (
+    <div><Comments /></div>
+  )
+}
+
+export default AdminComments
--- src/Admin/admin-pages/BlogsAdmin.tsx
+++ src/Admin/admin-pages/BlogsAdmin.tsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+
 
 const BlogsAdmin = () => {
+
+
+
   return (
     <div>
-    <p>Blog Admin Page</p>
-   
-</div>
+      <div>
+        <form>
+
+          {/*  Blog Titles  choose events or blogs*/}
+          <div> 
+            <label >Title:</label>
+            <div>
+              <input 
+                    type="text" 
+                    id="title" 
+                    name="title">
+                    </input>
+            </div>
+          </div>
+
+
+          {/* Blog Post */}
+          <div>
+            <label >Blog post:</label>
+            <div>
+              <textarea type="text" 
+                        id="blogs" 
+                        name="blogs">
+              </textarea>
+            </div>
+          </div>
+
+          {/* Upload Photo */}
+          <div>
+            <label ></label>
+            <h2> Add image:</h2>
+            <input type="file"></input>
+
+          </div>
+
+           {/* Post Button */}
+           <div>
+            <label ></label>
+            <button type="submit" className="mt-2 bg-red-400 rounded-lg p-1">Post</button>
+          </div>
+
+
+        </form>
+      </div>
+    </div>
   )
 }
 
