ebf1c0602f527c2b44f1cabb64708b86141cad5e
susheepls <169642127+susheepls@users.noreply.github.com>
2024-09-03 21:09:19 +0900

    Feat: add feature to display 5 randomly selected feedbacks

--- src/components/homeComponents/FeedbackComments.tsx
+++ src/components/homeComponents/FeedbackComments.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import * as feedbackApi from "../../api/feedbacks"
 import FeedbackType from '../../interfaces/FeedbackType';
+import Feedback from '../../interfaces/FeedbackType';
 
 
 interface ImageType {
@@ -22,10 +23,25 @@ const FeedbackComments: React.FC<Props> = (props) => {
     const fetchAllFeedbacks = async() => {
         const result = await feedbackApi.fetchAllFeedbacks();
         if(!result) return;
-        let filteredFeedback = result.filter((feedback) => feedback.is_accepted)
-        setFeedbacks(filteredFeedback);
+        const filteredFeedback = result.filter((feedback) => feedback.is_accepted)
+        let chosenFiveFeedbacks = (pickRandomElement(filteredFeedback));
+        setFeedbacks(chosenFiveFeedbacks);
     }
 
+    const pickRandomElement = (feedbackArr: Feedback[]) => {
+        let emptyArr: Feedback[] = [];
+        if(feedbackArr.length < 5){
+            emptyArr = feedbackArr;
+        } else {
+            while(emptyArr.length < 5){
+                let chosen = feedbackArr[Math.floor(Math.random() * feedbackArr.length)];
+                feedbackArr.splice(feedbackArr.indexOf(chosen), 1);
+                emptyArr.push(chosen);
+            }
+        }
+        return emptyArr;
+    }
+    
     const createFeedbackDiv = feedbacks.map((feedback, index) => {
         return <div id={"feedback-" + String(index+1)} key={feedback.name + index} className='m-4 flex flex-col justify-center'>
                 <div className="bg-white p-6 flex flex-col text-center text-md rounded-md drop-shadow-lg my-3">
