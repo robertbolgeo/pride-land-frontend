2fd712ad416eecf266201a3fd50bf6b92f636610
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-09-07 15:51:50 +0900

    Feat: added italian translation to homepage

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./i18n/locales/en.json";
 import jp from "./i18n/locales/jp.json";
+import it from "./i18n/locales/it.json";
 
 i18n.use(initReactI18next).init({
    fallbackLng: "jp",
@@ -12,6 +13,9 @@ i18n.use(initReactI18next).init({
       },
       jp: {
          translation: jp
+      },
+      it: {
+         translation: it
       }
    },
 });
