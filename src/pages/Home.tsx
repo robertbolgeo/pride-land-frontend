c3f584af01a948b88b6eb62ae481271a275e05d8
karl komeya <karlkomeya04@gmail.com>
2024-09-09 18:08:54 +0900

    feat: revised and created new blog post and text editor.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,9 +39,7 @@ function Home() {
       </section>
       <section id="blogs" className={"min-h-80 flex flex-col px-4"}>
         <div className="flex flex-col flex-1 max-w-[1400] mx-auto w-full"></div>
-        {/* <Blogs
-          images = { images }
-        /> */}
+        <Blogs />
       </section>
       <section id="kuchikomi" className={"min-h-80 flex px-4"}>
         <div className="flex max-w-[1400] mx-auto w-full ">
