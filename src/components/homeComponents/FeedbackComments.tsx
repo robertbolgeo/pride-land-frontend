d7c8390bc95b51c5c72ec7064b6dced6acc097e4
karl komeya <karlkomeya04@gmail.com>
2024-09-10 14:46:21 +0900

    chore: updated the ui features

--- src/components/homeComponents/FeedbackComments.tsx
+++ src/components/homeComponents/FeedbackComments.tsx
@@ -46,7 +46,7 @@ const FeedbackComments: React.FC<Props> = (props) => {
         return <div id={"feedback-" + String(index+1)} key={feedback.name + index} className='m-4 flex flex-col justify-center'>
                 <div className="bg-white p-6 flex flex-col text-center text-md rounded-md drop-shadow-lg my-3">
                     <div>
-                        <img src={props.images[index].src}></img>
+                        {/* <img src={props.images[index].src}></img> */}
                     </div>
                     <div className=''>
                         <h1>{feedback.comment}</h1>
--- src/components/homeComponents/Footer.tsx
+++ src/components/homeComponents/Footer.tsx
@@ -2,56 +2,55 @@ import { FaFacebookF, FaYoutube, FaGithub } from "react-icons/fa6";
 import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-   const { t } = useTranslation();
+  const { t } = useTranslation();
 
- return ( 
-  <footer className="w-full px-4 py-16 sm:py-20 md:px-8 md:py-24 bg-primary text-secondary">
-   <div className="max-w[1200px] mx-auto grid w-full grid-cols-1 gap-8 text-base sm:grid-cols-2 md:grid-cols-5">
-    <div className="flex flex-col gap-4 md:col-span-2">
-     <h1 className="font-semibold text-secondary">{t("footer.tm-header")}</h1>
-     <p>{t("footer.trademark")}</p>
-    </div>
-    <div className="flex flex-col gap-4">
-     <p className="text-base font-bold sm:text-lg">{t("footer.support")}</p>
-     <a
-      href="/contactus"
-      target="_blank"
-      className="cursor-pointer duration-150 hover:text-tertiary">
-      {t("footer.contact")}
-     </a>
-    </div>
-    <div className="flex flex-col gap-4">
-     <p className="text-base font-bold sm:text-lg">{t("footer.about")}</p>
-     <a
-      href="/aboutus"
-      target="_blank"
-      className="cursor-pointer duration-150 hover:text-tertiary">
-      {t("footer.mission")}
-     </a>
-    </div>
-    <div className="flex flex-col gap-4">
-     <p className="text-base font-bold sm:text-lg">{t("footer.follow")}</p>
-     <a
-      href="https://www.facebook.com/prideland4u"
-      className="cursor-pointer duration-150 hover:text-tertiary">
-      <FaFacebookF />
-      {t("footer.facebook")}
-     </a>
-     <a
-      href="https://github.com/pride-land"
-      className="cursor-pointer duration-150 hover:text-tertiary">
-      <FaGithub />
-      {t("footer.github")}
-     </a>
-     <a
-      href="https://www.youtube.com/channel/UCInYpCvSPY0ezO-gDb7R75Q"
-      target="_blank"
-      className="cursor-pointer duration-150 hover:text-blue-300">
-      <FaYoutube />
-      {t("footer.Youtube")}
-     </a>
-    </div>
-   </div>
-  </footer>
- );
+  return (
+    <footer className="w-full px-4 py-8 sm:py-12 md:px-8 md:py-16 
+      bg-gradient-to-br from-green-500 to-green-600 text-white">
+      <div className="max-w-6xl mx-auto flex flex-col gap-8 md:flex-row md:justify-between">
+        <div className="flex-1 flex flex-col gap-4">
+          <h1 className="text-xl font-semibold">{t("footer.tm-header")}</h1>
+          <p>{t("footer.trademark")}</p>
+        </div>
+        <div className="flex-1 flex flex-col gap-4">
+          <p className="text-lg font-semibold">{t("footer.support")}</p>
+          <a
+            href="/contactus"
+            className="text-white hover:text-yellow-300 transition-colors duration-200">
+            {t("footer.contact")}
+          </a>
+        </div>
+        <div className="flex-1 flex flex-col gap-4">
+          <p className="text-lg font-semibold">{t("footer.about")}</p>
+          <a
+            href="/aboutus"
+            className="text-white hover:text-yellow-300 transition-colors duration-200">
+            {t("footer.mission")}
+          </a>
+        </div>
+        <div className="flex-1 flex flex-col gap-4">
+          <p className="text-lg font-semibold">{t("footer.follow")}</p>
+          <div className="flex gap-4">
+            <a
+              href="https://www.facebook.com/prideland4u"
+              className="cursor-pointer duration-150 flex items-center text-white hover:text-yellow-300 transition-colors ">
+              <FaFacebookF className="text-xl mr-2" />
+              {t("footer.facebook")}
+            </a>
+            <a
+              href="https://github.com/pride-land"
+              className="cursor-pointer duration-150  flex items-center text-white hover:text-yellow-300 transition-colors ">
+              <FaGithub className="text-xl mr-2" />
+              {t("footer.github")}
+            </a>
+            <a href="https://www.youtube.com/channel/UCInYpCvSPY0ezO-gDb7R75Q"
+              className="cursor-pointer duration-150 flex items-center text-white hover:text-yellow-300 transition-colors ">
+              <FaYoutube className="text-xl mr-2" />
+              {t("footer.Youtube")}
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
